73479684b1e0de2f4aeb414db9f9a4d2e8b1d548
NaiveDJack <naivedjack@protonmail.com>
2021-05-10 12:14:19 +0200

    operators logic and precedence

---
+++ scripts/script.js
@@ -0,0 +1,57 @@
+//@ts-check
+function add (num1, num2) {
+	return num1 + num2
+}
+
+function subtract (num1, num2) {
+	return num1 - num2	
+}
+
+function multiply (num1, num2) {
+    return num1 * num2
+}
+
+function divide (num1, num2) {
+    return num1 / num2
+}
+
+function operate (operator, num1, num2) {
+    if (operator === 'add') {
+        return add(num1, num2);
+    }
+    if (operator === 'subtract') {
+        return subtract(num1, num2);
+    }
+    if (operator === 'multiply') {
+        return multiply(num1, num2);
+    }
+    if (operator === 'divide') {
+        return divide(num1, num2);
+    }
+}
+
+function operateMultiple (array) {
+    while (array.length > 1) {
+        if (array.includes('divide')) {
+            let pos = array.findIndex(element => (element == 'divide'));
+            let stepResult = operate(array[pos], array[pos - 1], array[pos + 1]);
+            array.splice(pos - 1, 3, stepResult);
+        }
+        else if (array.includes('multiply')) {
+            let pos = array.findIndex(element => (element == 'multiply'));
+            let stepResult = operate(array[pos], array[pos - 1], array[pos + 1]);
+            array.splice(pos - 1, 3, stepResult);
+        }
+        else if (array.includes('subtract')) {
+            let pos = array.findIndex(element => (element == 'subtract'));
+            let stepResult = operate(array[pos], array[pos - 1], array[pos + 1]);
+            array.splice(pos - 1, 3, stepResult);
+        }
+        else if (array.includes('add')) {
+            let pos = array.findIndex(element => (element == 'add'));
+            let stepResult = operate(array[pos], array[pos - 1], array[pos + 1]);
+            array.splice(pos - 1, 3, stepResult);
+        }
+    }
+    
+}
